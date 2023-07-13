49c8409babeb081ad0d14ce59034e14011d9d00c
Medea <logmedha09@gmail.com>
2023-07-13 18:07:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,27 +2,24 @@ const axios = require('axios');
 const fs = require('fs');
 
 axios.get("https://backend-omega-seven.vercel.app/api/getjoke")
-  .then(res => {
-    const data = res.data;
-    const question = data[0].question;
-    const punchline = data[0].punchline;
- 
-    fs.readFile('README.md', 'utf-8', function(err, data) {
-        if (err) throw err;
-    
-        var text = `
+.then(res => {
+  const data = res.data;
+  const question = data[0].question;
+  const punchline = data[0].punchline;
+
+  
+  var text = `
 # Daily Dev Joke
 
 - Fetches a joke from an API everyday and edits this README`
-        text += "\n\n**" + question + "**"
-        text += "\n*" + punchline + "*"
-    
-        fs.writeFile('README.md', text, 'utf-8', function(err, data) {
-            if (err) throw err;
-            console.log('Done!');
-        })
-    })
+  text += "\n\n**" + question + "**"
+  text += "\n*" + punchline + "*"
+
+  fs.writeFile('README.md', text, 'utf-8', function(err, data) {
+      if (err) throw err;
+      console.log('Done!');
   })
-  .catch(err => {
-    console.log('Error: ', err.message);
-  });
+})
+.catch(err => {
+  console.log('Error: ', err.message);
+});
