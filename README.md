543432ea8f181babb6cb65c1d4721408ce1cacca
VulcanWM <logmedha09@gmail.com>
2023-07-13 16:26:27 +0100

    added the getting joke and edit readme script

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+
 # Daily Dev Joke
 
 - Fetches a joke from an API everyday and edits this README
+
+**Why are keyboards always working so hard?**
+*Cause they have two shifts.*
---
+++ index.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+
+axios.get("https://backend-omega-seven.vercel.app/api/getjoke")
+  .then(res => {
+    const data = res.data;
+    const question = data[0].question;
+    const punchline = data[0].punchline;
+    console.log(question)
+    console.log(punchline)
+    var fs = require('fs');
+ 
+    fs.readFile('README.md', 'utf-8', function(err, data) {
+        if (err) throw err;
+    
+        var text = `
+# Daily Dev Joke
+
+- Fetches a joke from an API everyday and edits this README`
+        text += "\n\n**" + question + "**"
+        text += "\n*" + punchline + "*"
+    
+        fs.writeFile('README.md', text, 'utf-8', function(err, data) {
+            if (err) throw err;
+            console.log('Done!');
+        })
+    })
+  })
+  .catch(err => {
+    console.log('Error: ', err.message);
+  });
