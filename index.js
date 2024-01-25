e38cf13c78f37cbd4ccf4625b687e71c9548d1f5
Medea <logmedha09@gmail.com>
2024-01-25 18:19:14 +0000

    added 2part to url

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const fs = require('fs');
 
-axios.get("https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit")
+axios.get("https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=twopart")
 .then(res => {
   const data = res.data;
   const question = data[0].setup;
